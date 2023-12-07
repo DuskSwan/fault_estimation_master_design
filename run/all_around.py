563bc315b1ac5a320ec2a6e869d6141f9167a62b
DuskSwan <aquark@foxmail.com>
2023-12-07 16:21:33 +0800

    add loguru

--- run/all_around.py
+++ run/all_around.py
@@ -3,6 +3,8 @@
 实现时频域提取特征与预测的接驳
 """
 import sys
+from pathlib import Path
+from loguru import logger
 import time
 import random
 
@@ -17,3 +19,26 @@ from config import cfg
 from utils import set_random_seed
 
 set_random_seed(0)
+
+def main(priv_cfg_path):
+    cfg.merge_from_file(priv_cfg_path)
+    cfg.freeze()
+
+    output_dir = Path(cfg.OUTPUT_DIR)
+    if not output_dir.exists: output_dir.mkdir()
+
+    cur_time = time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime())
+    logger.add(str(cfg.LOG_DIR / f'{cur_time}.log'), rotation='1 day', encoding='utf-8')
+    logger.info("In device {}".format(cfg.MODEL.DEVICE))
+
+    logger.info("Running with config:\n{}".format(cfg))
+
+    model = build_model(cfg)
+    model.load_state_dict(torch.load(cfg.TEST.WEIGHT))
+    val_loader = make_data_loader(cfg, is_train=False)
+
+    inference(cfg, model, val_loader)
+
+
+if __name__ == '__main__':
+    main()
