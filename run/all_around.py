d2443a1ff5a26db05b1bf20deb1d847976abbb56
DuskSwan <aquark@buaa.edu.cn>
2023-12-14 16:02:47 +0800

    construct run.tools

--- run/all_around.py
+++ run/all_around.py
@@ -12,6 +12,7 @@ import numpy as np
 import torch.nn as nn
 
 sys.path.append('.')
+from run.tools import signal_to_XY
 from config import cfg
 from utils import set_random_seed, sheet_cut
 from utils.features import view_features_DTW, signal_to_features_tf
@@ -22,41 +23,10 @@ from solver import make_optimizer
 
 
 def train(cfg):
-    # 设置参数
-    normal_data = pd.read_csv(cfg.TRAIN.NORMAL_PATH).values #读成numpy数组
-    n,_ = normal_data.shape # 1958912
-    piece = cfg.DESIGN.PIECE
-    subl = cfg.DESIGN.SUBLEN
-    
-    # 判断采样点数是否足够
-    assert n > piece + subl, f"原始采样点数为{n}，所需采样点数为{piece}+{subl}={piece+subl}，无法达标"
-    
-    # 特征提取
-    logger.info('start to extract features...')
-    XY = sheet_cut(normal_data, subl, piece, method = 3, show_para = True)
-        # XY是时间序列切片得到的样本集，是三维数组（单个样本是二维数组），每个样本计算一个向量作为特征
-        # 3意味着这一步需要保证特征是连续提取的
-    st = time.time()
-    normal_f = signal_to_features_tf(XY,feat_func_name_list = cfg.FEATURE.USED_F) 
-        # 形状(piece,p_feature)
-    et = time.time()
-    logger.info('feature extraction finished with time {:.6f}s.'.format(et-st))
-    logger.info('features shape: {}'.format(normal_f.shape))
-
-    # 特征划分
-    f_dataset = sheet_cut(normal_f, sublen=cfg.DESIGN.FSUBLEN, piece=cfg.DESIGN.FPIECE, method=1) 
-        #再次切分成小数据集，现在是三维数组
-    
-    #制作X与Y
-    logger.info('start to make X and Y...')
-    X = []
-    Y = []
-    m = cfg.DESIGN.M
-    for signal in f_dataset:
-        X.append(signal[:m,:][:])
-        Y.append(signal[m:,:][:])
-    X,Y = map(np.array,(X,Y))
-    logger.info('X:{} Y:{}'.format(X.shape,Y.shape))
+    # get data
+    X,Y = signal_to_XY(cfg)
+    train_loader = make_data_loader(cfg, X,Y, is_train=True)
+    val_loader = make_data_loader(cfg, X,Y, is_train=False)
 
     # GET MODEL
     _, in_len, in_tunnel = X.shape
@@ -68,12 +38,10 @@ def train(cfg):
     model = build_model(cfg, net_params)
     logger.info('Get model with params: {}'.format(net_params))
 
-    # 读取设置
+    # get solver
     optimizer = make_optimizer(cfg, model)
     scheduler = None
 
-    train_loader = make_data_loader(cfg, X,Y, is_train=True)
-    val_loader = make_data_loader(cfg, X,Y, is_train=False)
 
     do_train(
         cfg,
